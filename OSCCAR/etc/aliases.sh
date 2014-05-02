6445e115690533512c6a18c942f028b452e10591
gijsbert <gijsbert>
2014-05-02 09:09:08 +0200

    Fixed and expanded OSCCAR environment with build scripts

---
+++ OSCCAR/etc/aliases.sh
@@ -0,0 +1,15 @@
+# OSCCAR aliases
+
+
+# Create some aliases for the OSCCAR library
+alias otut='cd $OSCCAR_TUTORIALS'
+alias osrc='cd $OSCCAR_SRC'
+alias osol='cd $OSCCAR_SOLVERS'
+alias outil='cd $OSCCAR_UTILITIES'
+alias osccar='cd $OSCCAR_HOME'
+
+# Create some aliases for the OSCCAR library
+alias osccartut='cd $OSCCAR_TUTORIALS'
+alias osccarsrc='cd $OSCCAR_SRC'
+alias osccarsol='cd $OSCCAR_SOLVERS'
+alias osccarutil='cd $OSCCAR_UTILITIES'
