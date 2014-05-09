3965837a7ea2825d6e241e267456544f8dd1a486
gijsbert <gijsbert>
2014-05-09 10:46:48 +0200

    Add odoc and osccardoc aliases to get to OSCCAR documentation dir

--- OSCCAR/etc/aliases.sh
+++ OSCCAR/etc/aliases.sh
@@ -6,6 +6,7 @@ alias otut='cd $OSCCAR_TUTORIALS'
 alias osrc='cd $OSCCAR_SRC'
 alias osol='cd $OSCCAR_SOLVERS'
 alias outil='cd $OSCCAR_UTILITIES'
+alias odoc='cd $OSCCAR_DOC'
 alias osccar='cd $OSCCAR_HOME'
 
 # Create some aliases for the OSCCAR library
@@ -13,3 +14,4 @@ alias osccartut='cd $OSCCAR_TUTORIALS'
 alias osccarsrc='cd $OSCCAR_SRC'
 alias osccarsol='cd $OSCCAR_SOLVERS'
 alias osccarutil='cd $OSCCAR_UTILITIES'
+alias osccardoc='cd $OSCCAR_DOC'
